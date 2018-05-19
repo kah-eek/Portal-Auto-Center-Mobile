ec7c14ed0ce1edc523f66facddfabb4df23b923b
Caique M. Oliveira <caique.m.oliveira.br@gmail.com>
2018-05-19 15:00:01 -0300

    login screen layout finished

--- www/js/utils.js
+++ www/js/utils.js
@@ -1,6 +1,7 @@
 $(function(){
-	// Load header on page
-	$('.header').load('header.html',function(){
+
+	// Load navigator header on page
+	$('.header').load('nav_header.html',function(){
 	  // Open hamburger menu when click on it
       $('#menu').click(function(){
         $('.hamburger-container').animate({left:'100%'},800);
@@ -11,5 +12,9 @@ $(function(){
         $('.hamburger-container').animate({left:'-100%'},800);
       });
 	});
+  // *********************************************
+  
+  // Load header no navigation on page
+  $('.no-nav-header').load('only_header.html');
 	// *********************************************
 });
