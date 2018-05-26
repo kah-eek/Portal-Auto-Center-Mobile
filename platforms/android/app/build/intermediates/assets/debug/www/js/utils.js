ce39e88f3dddbc1eead63248a3c71a9f67a9c3e6
Caique M. Oliveira <caique.m.oliveira.br@gmail.com>
2018-05-26 00:33:17 -0300

    get states and show in combo box

--- platforms/android/app/build/intermediates/assets/debug/www/js/utils.js
+++ platforms/android/app/build/intermediates/assets/debug/www/js/utils.js
@@ -1,20 +1,13 @@
-$(function(){
-
-	// Load navigator header on page
-	$('.header').load('nav_header.html',function(){
-	  // Open hamburger menu when click on it
-      $('#menu').click(function(){
-        $('.hamburger-container').animate({left:'100%'},800);
-      });
-
-      // Close hamburger menu when click on it (dark side)
-      $('#close-menu').click(function(){
-        $('.hamburger-container').animate({left:'-100%'},800);
-      });
-	});
-  // *********************************************
-  
-  // Load header no navigation on page
-  $('.no-nav-header').load('only_header.html');
-	// *********************************************
-});
+/**
+* Get variable on URL
+* @param variable Variable to get on url
+* @return String Variable's value
+*/
+function getVariable(variable)
+{	
+	var urlVariables = window.location.href.substring(window.location.href.search('\\?'));
+	var startIndex = urlVariables.search(variable.substring(variable.length-1)+'=');
+	var variableValue = urlVariables.substring(startIndex+2);
+	// console.log(variableValue);
+	return variableValue;
+}
--- www/js/api.js
+++ www/js/api.js
@@ -4,5 +4,6 @@ var host = '127.0.0.1';
 	
 // API Routes 
 var api = {
-	'authentication':`http://${host}/Portal-Auto-Center-API/api/v1/user/`
+	'authentication':`http://${host}/Portal-Auto-Center-API/api/v1/user/`,
+	'state':`http://${host}/Portal-Auto-Center-API/api/v1/state/`
 }
