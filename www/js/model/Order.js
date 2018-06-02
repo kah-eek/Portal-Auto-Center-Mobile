8def558aa5a502640ff8a60996a9f2e2b910fddb
Caique M. Oliveira <caique.m.oliveira.br@gmail.com>
2018-06-02 20:32:39 -0300

    make order request on internal system - pac

--- www/js/model/Order.js
+++ www/js/model/Order.js
@@ -22,6 +22,37 @@ class Order
 	}
 
 	/**
+	* Create a orders list in local storage
+	*/
+	static createOrderGroup()
+	{
+		// Create a list with Order objectes
+        localStorage.setItem('orderGroup',JSON.stringify([]));		
+	}
+
+	/**
+	* Get the order group
+	* @return array Order objects array on JSON format
+	*/
+	static getOrderGroup()
+	{
+		return JSON.parse(localStorage.getItem('orderGroup'));
+	}
+
+	/**
+	* Add a new order to order list
+	* @param orderObj Order that will insert into orders list
+	*/
+	static addOrderToGroup(orderObj)
+	{
+		var orderGroup = JSON.parse(localStorage.getItem('orderGroup'));
+
+        orderGroup.push({'order':orderObj});
+
+        localStorage.setItem('orderGroup',JSON.stringify(orderGroup));
+	}
+
+	/**
 	* Insert a new order register into database
 	* @param orderObj Object that will be inserted into database
 	* @param callbackSuccess(data) Callback executed in success on get data 
