6ae0bf82fc2afa073e4f6d640be59eda036c2087
Caique M. Oliveira <caique.m.oliveira.br@gmail.com>
2018-06-04 00:47:21 -0300

    alter APIs route to remote route

--- www/js/api.js
+++ www/js/api.js
@@ -1,21 +1,39 @@
 // Host
 // var host = '192.168.0.100';
 // var host = '192.168.0.110';
-var host = '127.0.0.1';
+var host = 'www.autofast.com';
+// var host = '127.0.0.1';
+// var host = 'caiqueoliveira1.tempsite.ws';
 	
 // API Routes 
 var api = {
-	'authentication':`http://${host}/Portal-Auto-Center-API/api/v1/user/`,
-	'state':`http://${host}/Portal-Auto-Center-API/api/v1/state/`,
-	'order':`http://${host}/Portal-Auto-Center-API/api/v1/order/`,
-	'product':`http://${host}/Portal-Auto-Center-API/api/v1/product/`,
-	'vehicle':`http://${host}/Portal-Auto-Center-API/api/v1/vehicle/`,
-	'fuel':`http://${host}/Portal-Auto-Center-API/api/v1/fuel/`,
-	'fuel_supply':`http://${host}/Portal-Auto-Center-API/api/v1/fuel/supply/`,
-	'service':`http://${host}/Portal-Auto-Center-API/api/v1/service/`,
-	'service_details':`http://${host}/Portal-Auto-Center-API/api/v1/service/details/`,
-	'service_provider':`http://${host}/Portal-Auto-Center-API/api/v1/service/provider/`,
-	'supply_register':`http://${host}/Portal-Auto-Center-API/api/v1/supplyRegister/`,
-	'payment':`http://${host}/Portal-Auto-Center-API/api/v1/payment/`,
+	'authentication':`http://${host}/api/v1/user/`,
+	'state':`http://${host}/api/v1/state/`,
+	'order':`http://${host}/api/v1/order/`,
+	'product':`http://${host}/api/v1/product/`,
+	'vehicle':`http://${host}/api/v1/vehicle/`,
+	'fuel':`http://${host}/api/v1/fuel/`,
+	'fuel_supply':`http://${host}/api/v1/fuel/supply/`,
+	'service':`http://${host}/api/v1/service/`,
+	'service_details':`http://${host}/api/v1/service/details/`,
+	'service_provider':`http://${host}/api/v1/service/provider/`,
+	'supply_register':`http://${host}/api/v1/supplyRegister/`,
+	'payment':`http://${host}/api/v1/payment/`,
 	'google_geocode':`https://maps.googleapis.com/maps/api/geocode/json?latlng=`
 };
+
+// var api = {
+// 	'authentication':`http://${host}/Portal-Auto-Center-API/api/v1/user/`,
+// 	'state':`http://${host}/Portal-Auto-Center-API/api/v1/state/`,
+// 	'order':`http://${host}/Portal-Auto-Center-API/api/v1/order/`,
+// 	'product':`http://${host}/Portal-Auto-Center-API/api/v1/product/`,
+// 	'vehicle':`http://${host}/Portal-Auto-Center-API/api/v1/vehicle/`,
+// 	'fuel':`http://${host}/Portal-Auto-Center-API/api/v1/fuel/`,
+// 	'fuel_supply':`http://${host}/Portal-Auto-Center-API/api/v1/fuel/supply/`,
+// 	'service':`http://${host}/Portal-Auto-Center-API/api/v1/service/`,
+// 	'service_details':`http://${host}/Portal-Auto-Center-API/api/v1/service/details/`,
+// 	'service_provider':`http://${host}/Portal-Auto-Center-API/api/v1/service/provider/`,
+// 	'supply_register':`http://${host}/Portal-Auto-Center-API/api/v1/supplyRegister/`,
+// 	'payment':`http://${host}/Portal-Auto-Center-API/api/v1/payment/`,
+// 	'google_geocode':`https://maps.googleapis.com/maps/api/geocode/json?latlng=`
+// };
